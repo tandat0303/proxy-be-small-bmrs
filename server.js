d0b1875f78ba883af4965f62b40079780c263cb5
tandat0303 <ls17189a3.11@gmail.com>
2026-02-04 14:21:26 +0700

    adjust CORS

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://small-bmrs.vercel.app",
+    credentials: true,
+  }),
+);
 
 app.use(
   "/api",
