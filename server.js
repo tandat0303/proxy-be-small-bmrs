1875f7a23371f2406e37c4229e2b1f491b82350a
tandat0303 <ls17189a3.11@gmail.com>
2026-02-04 13:44:19 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://erp.lacty.com.vn:8081",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("ERP Proxy Server Running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Proxy running on port", PORT));
